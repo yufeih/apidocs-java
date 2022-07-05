f5773603dd3bda01a30a9466cf8a9ce09002927f
Yufei Huang <yufeih@live.com>
2022-07-05 11:44:23 +0800

    jsdoc

---
+++ typescript/index.js
@@ -0,0 +1,15 @@
+/**
+ * Renders an entire login page with email and password fields
+ * using {@link Renderer}.
+ *
+ * Used by external plugins
+ *
+ * @param  {Object} options
+ * @param  {String} options.action          login form action
+ * @param  {String} [options.errorMessage]  optional messaga
+ * @return {Promise<string>}                HTML of the page
+ */
+function renderLogin(_a) {
+    var action = _a.action, errorMessage = _a.errorMessage;
+    return '';
+}
